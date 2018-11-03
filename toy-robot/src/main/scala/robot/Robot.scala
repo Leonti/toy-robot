ca433c283159ac39edc9b38a702e57dc48827031
Leonti <prishelec@gmail.com>
2018-11-03 18:45:37 +1100

    Add reporting

--- toy-robot/src/main/scala/robot/Robot.scala
+++ toy-robot/src/main/scala/robot/Robot.scala
@@ -22,32 +22,33 @@ object Robot {
   final case object NotPlaced                  extends Position
   case class OnGrid(x: Int, y: Int, f: Facing) extends Position
 
-  type App = State[Position, Position]
+  sealed trait Op
+  final case object NoOp extends Op
+  final case class ReportOp(p: Position) extends Op
+
+  type App = State[Position, Op]
 
   private def processCommand(size: Int)(command: Command): App = State { position =>
-    val next = command match {
-      case Place(x, y, f) => OnGrid(x, y, f)
+    command match {
+      case Place(x, y, f) if x >= 0 && x < size && y >= 0 && y < size => (OnGrid(x, y, f), NoOp)
       case Move =>
         position match {
           case OnGrid(x, y, f) =>
             f match {
-              case North if (y + 1) < size => OnGrid(x, y + 1, f)
-              case East if (x + 1) < size => OnGrid(x + 1, y, f)
-              case South if (y - 1) >= 0 => OnGrid(x, y - 1, f)
-              case West if (x - 1) >= 0 => OnGrid(x - 1, y, f)
-              case _ => position
+              case North if y + 1 < size => (OnGrid(x, y + 1, f), NoOp)
+              case East if x + 1 < size => (OnGrid(x + 1, y, f), NoOp)
+              case South if y - 1 >= 0 => (OnGrid(x, y - 1, f), NoOp)
+              case West if x - 1 >= 0 => (OnGrid(x - 1, y, f), NoOp)
+              case _ => (position, NoOp)
             }
-          case NotPlaced => position
+          case NotPlaced => (position, NoOp)
         }
-      case _ => position
+      case Report => (position, ReportOp(position))
+      case _ => (position, NoOp)
     }
-
-    (next, next)
   }
 
-  def move(size: Int, commands: List[Command]): Position = {
-    val (position, _) = commands.traverse(processCommand(size)).run(NotPlaced).value
+  case class Result(pos: Position, ops: List[Op])
 
-    position
-  }
+  def move(size: Int, commands: List[Command]): Result = Result.tupled(commands.traverse(processCommand(size)).run(NotPlaced).value)
 }
--- toy-robot/src/test/scala/RobotSpec.scala
+++ toy-robot/src/test/scala/RobotSpec.scala
@@ -4,45 +4,65 @@ import robot.Robot._
 
 class RobotSpec extends FlatSpec with Matchers {
 
-  behavior of "robot"
+  behavior of "placing"
 
   it should "have unknown position if not placed" in {
-   Robot.move(5, List()) shouldBe NotPlaced
+   Robot.move(5, List()).pos shouldBe NotPlaced
   }
 
   it should "have placed position" in {
-    Robot.move(5, List(Place(0, 0, North))) shouldBe OnGrid(0, 0, North)
+    Robot.move(5, List(Place(0, 0, North))).pos shouldBe OnGrid(0, 0, North)
   }
 
+  it should "not place outside of the grid" in {
+    Robot.move(5, List(Place(5, 5, North))).pos shouldBe NotPlaced
+  }
+
+  behavior of "moving"
+
   it should "move North" in {
-    Robot.move(5, List(Place(2, 2, North), Move)) shouldBe OnGrid(2, 3, North)
+    Robot.move(5, List(Place(2, 2, North), Move)).pos shouldBe OnGrid(2, 3, North)
   }
 
   it should "move East" in {
-    Robot.move(5, List(Place(2, 2, East), Move)) shouldBe OnGrid(3, 2, East)
+    Robot.move(5, List(Place(2, 2, East), Move)).pos shouldBe OnGrid(3, 2, East)
   }
 
   it should "move South" in {
-    Robot.move(5, List(Place(2, 2, South), Move)) shouldBe OnGrid(2, 1, South)
+    Robot.move(5, List(Place(2, 2, South), Move)).pos shouldBe OnGrid(2, 1, South)
   }
 
   it should "move West" in {
-    Robot.move(5, List(Place(2, 2, West), Move)) shouldBe OnGrid(1, 2, West)
+    Robot.move(5, List(Place(2, 2, West), Move)).pos shouldBe OnGrid(1, 2, West)
   }
 
   it should "not move outside the grid when moving North" in {
-    Robot.move(5, List(Place(0, 4, North), Move)) shouldBe OnGrid(0, 4, North)
+    Robot.move(5, List(Place(0, 4, North), Move)).pos shouldBe OnGrid(0, 4, North)
   }
 
   it should "not move outside the grid when moving East" in {
-    Robot.move(5, List(Place(4, 0, East), Move)) shouldBe OnGrid(4, 0, East)
+    Robot.move(5, List(Place(4, 0, East), Move)).pos shouldBe OnGrid(4, 0, East)
   }
 
   it should "not move outside the grid when moving West" in {
-    Robot.move(5, List(Place(0, 0, West), Move)) shouldBe OnGrid(0, 0, West)
+    Robot.move(5, List(Place(0, 0, West), Move)).pos shouldBe OnGrid(0, 0, West)
   }
 
   it should "not move outside the grid when moving South" in {
-    Robot.move(5, List(Place(0, 0, South), Move)) shouldBe OnGrid(0, 0, South)
+    Robot.move(5, List(Place(0, 0, South), Move)).pos shouldBe OnGrid(0, 0, South)
+  }
+
+  behavior of "reporting"
+
+  it should "report unplaced position" in {
+    Robot.move(5, List(Report)).ops shouldBe List(ReportOp(NotPlaced))
+  }
+
+  it should "report placed position" in {
+    Robot.move(5, List(Place(0, 0, South), Report)).ops shouldBe List(NoOp, ReportOp(OnGrid(0, 0, South)))
+  }
+
+  it should "not report if not requested" in {
+    Robot.move(5, List()).ops shouldBe List()
   }
 }
